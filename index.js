6b5f3e367ae66ec1f70208663a95a4d255cb1297
tpreisig <90914512+tpreisig@users.noreply.github.com>
2024-01-28 20:21:56 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import exp from "constants";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const app = express();
+const port = 3000;
+
+var userIsAuthorised;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+function magicWordChecker(req, res, next) {
+    const password = req.body["password"];
+    if (password === "Magician") {
+        userIsAuthorised = true;
+    } else {
+        userIsAuthorised = false;
+    }
+    console.log(req.body["password"]);
+    next();
+}
+app.use(magicWordChecker);
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/index.html");
+});
+
+app.post("/check", (req, res) => {
+    if (userIsAuthorised) {
+        res.sendFile(__dirname + "/views/secret.html");
+    } else {
+        res.sendFile(__dirname + "/views/index.html");
+    }
+});
+
+app.listen(port, () => {
+    console.log(`App is listening on port ${port}.`);
+});
+
+
